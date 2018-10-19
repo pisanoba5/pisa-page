70f3d4b5a12871ca4c11871a2ec10288bbd71332
pisanoba5 <seungpro0922@gamil.com>
2018-10-19 23:10:01 +0900

    initial commit

---
+++ src/main/java/net/pisa/PisaPageApplication.java
@@ -0,0 +1,12 @@
+package net.pisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PisaPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PisaPageApplication.class, args);
+	}
+}
